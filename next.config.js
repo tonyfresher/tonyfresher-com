b411427bdaefde9c59daea7c0a376b8b031b90bd
Anton Fresher <fresher@yandex-team.ru>
2021-01-16 23:39:55 +0300

    Add redirects

--- next.config.js
+++ next.config.js
@@ -1,4 +1,43 @@
 module.exports = {
+    redirects: () => {
+        return [
+            {
+                source: '/blog/the-storage',
+                destination: '/posts/the-storage',
+                permanent: true
+            },
+            {
+                source: '/blog/mountain-tracking',
+                destination: '/posts/mountain-tracking',
+                permanent: true
+            },
+            {
+                source: '/blog/sputnik-v',
+                destination: '/posts/sputnik-v',
+                permanent: true
+            },
+            {
+                source: '/blog/books',
+                destination: '/books',
+                permanent: true
+            },
+            {
+                source: '/blog/flow',
+                destination: '/books/flow',
+                permanent: true
+            },
+            {
+                source: '/blog/mindset',
+                destination: '/books/mindset',
+                permanent: true
+            },
+            {
+                source: '/blog/yasno-ponyatno',
+                destination: '/books/yasno-ponyatno',
+                permanent: true
+            }
+        ];
+    },
     webpack: config => {
         config.module.rules.push({
             test: /\.md$/,
