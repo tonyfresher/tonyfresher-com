1a3ea86601a510b7d6eada2baf656c33e75245dc
Anton Fresher <farnetsohn@gmail.com>
2022-08-06 16:29:57 +0400

    Fix

--- components/Home/i18n/en.ts
+++ components/Home/i18n/en.ts
@@ -4,7 +4,7 @@ Hey, I'm Anton. I'm a product designer and developer. I'm currently building a c
 
 Before Practicum, I was a web developer at [Yandex Surveys](https://surveys.yandex.com), a research tool for conducting surveys. Earlier—[Yandex Weather iOS app](https://apps.apple.com/us/app/yandex-weather-online-forecast/id1041008044), Math+CS at UrFU.
 
-I also irregularly write and play on my [Telegram channel](https://t.me/+TT5uDGL6_txhZTRi) (ru).
+I also irregularly write and play on my [Telegram channel](https://t.me/+TT5uDGL6_txhZTRi) (on russian).
     `,
     menu: [
         {label: 'Anton Fresher', link: '/'},
