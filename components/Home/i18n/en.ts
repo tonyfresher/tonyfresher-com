4f6a0325055b09a19c5df93299b47ffa21cf83dd
Anton Fresher <farnetsohn@gmail.com>
2022-08-06 16:27:18 +0400

    Fix about text

--- components/Home/i18n/en.ts
+++ components/Home/i18n/en.ts
@@ -1,8 +1,8 @@
 export default {
     about: `
-Hey, I'm Anton. I'm a product designer and developer. I'm currently building a core learning platform at [Practicum](https://practicum.com), a family of digital re-skilling products to help people build sustainable careers in IT.
+Hey, I'm Anton. I'm a product designer and developer. I'm currently building a core learning platform at [Practicum](https://practicum.com), online part-time coding bootcamps.
 
-Before Practicum, I was a web developer at [Yandex Surveys](https://surveys.yandex.com), a tool for researchers where you can simply create a survey and find a suitable audience for it. Earlier—[Yandex Weather iOS app](https://apps.apple.com/us/app/yandex-weather-online-forecast/id1041008044), Math+CS at UrFU.
+Before Practicum, I was a web developer at [Yandex Surveys](https://surveys.yandex.com), a research tool for conducting surveys. Earlier—[Yandex Weather iOS app](https://apps.apple.com/us/app/yandex-weather-online-forecast/id1041008044), Math+CS at UrFU.
 
 I also irregularly write and play on my [Telegram channel](https://t.me/+TT5uDGL6_txhZTRi) (ru).
     `,
