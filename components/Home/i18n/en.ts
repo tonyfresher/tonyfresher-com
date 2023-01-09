8bc292a73954ecb3e8233e67ba38647d2dc92ca3
Anton Fresher <farnetsohn@gmail.com>
2023-01-10 00:50:44 +0400

    Fix

--- components/Home/i18n/en.ts
+++ components/Home/i18n/en.ts
@@ -2,7 +2,7 @@ export default {
     about: `
 Hey! I’m Anton. Generally, you would see me jumping in between design, software engineering, product management, leadership, and writing, still figuring out what to do with all this.
 
-I'm currently building a core platform at [Practicum](https://practicum.com), a beginner-friendly online coding bootcamp.
+Now I'm building a core platform at [Practicum](https://practicum.com), a beginner-friendly online coding bootcamp.
 
 Before Practicum, I was a software engineer at [Yandex](https://yandex.com). Earlier—got a BSc in math and computer science at the Ural Federal University.
     `,
