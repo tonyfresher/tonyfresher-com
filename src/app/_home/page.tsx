9906149ee2a10757820b179f59460516e7d1b8bd
Anton Frehser <hey@tonyfresher.com>
2025-09-30 03:09:22 +0200

    Add name

--- src/app/_home/page.tsx
+++ src/app/_home/page.tsx
@@ -5,6 +5,10 @@ import Education from './components/education'
 import Footer from './components/footer.mdx'
 import Work from './components/work'
 
+export const strings = {
+    name: 'Anton Fresher'
+}
+
 export default function HomePage() {
     return (
         <div className="col-span-4 flex flex-col gap-y-24 text-2xl">
@@ -16,6 +20,7 @@ export default function HomePage() {
                     '[&_img]:mx-1 [&_img]:inline-block [&_img]:size-8 [&_img]:rounded-md [&_img]:align-text-top'
                 )}
             >
+                <h1 className="col-span-5 m-0 mb-10 text-4xl font-semibold">{strings.name}</h1>
                 <About />
             </div>
             <Work />
