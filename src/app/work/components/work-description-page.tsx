e6c34e0f21632d3ea32b3cc02e2eda90a2a877e9
Anton Frehser <hey@tonyfresher.com>
2025-10-20 02:32:57 +0200

    Fixes

--- src/app/work/components/work-description-page.tsx
+++ src/app/work/components/work-description-page.tsx
@@ -23,10 +23,10 @@ export default function WorkDescriptionPage({
                 '[&>*:not(.wide-block)]:col-span-4'
             )}
         >
-            <h1 className="font-display font-regular col-span-5 m-0 text-[56px] leading-none">
+            <h1 className="font-display font-regular col-span-5 m-0 text-[52px] leading-none">
                 {product}
             </h1>
-            <div className="text-muted-foreground col-span-5 mt-10 mb-8 flex flex-col gap-1">
+            <div className="text-muted-foreground col-span-5 mt-12 mb-8 flex flex-col gap-1">
                 <div>{period}</div>
                 <div>
                     <Link href={link} display="inline-block">
