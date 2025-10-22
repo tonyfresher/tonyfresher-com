083e36c05d0dcaa71703e699734a3e291f8738cd
Anton Frehser <hey@tonyfresher.com>
2025-10-22 13:47:32 +0200

    Add icons

--- src/app/_home/components/work.tsx
+++ src/app/_home/components/work.tsx
@@ -1,3 +1,5 @@
+import { ArrowSquareUpRight, Link01 } from '@untitledui/icons'
+
 import Link from '@/components/link'
 
 const strings = {
@@ -56,7 +58,17 @@ export default function Work() {
                     <div
                         className={`col-start-1 row-start-1 flex flex-col ${cover ? 'max-w-80 max-lg:max-w-full' : ''}`}
                     >
-                        <h3 className="m-0 font-semibold">{product}</h3>
+                        <h3 className="m-0 font-semibold">
+                            {product}
+                            {link.startsWith('/') ? (
+                                <ArrowSquareUpRight
+                                    className="ml-1.5 inline size-5"
+                                    strokeWidth={2.6}
+                                />
+                            ) : (
+                                <Link01 className="ml-1.5 inline size-5" strokeWidth={2.6} />
+                            )}
+                        </h3>
                         <span className="mt-1 whitespace-pre-line max-sm:whitespace-normal">
                             {description}
                         </span>
