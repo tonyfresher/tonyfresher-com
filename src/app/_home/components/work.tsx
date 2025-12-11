c325262aba18a201e47ad8c63c51503f89ecfd1f
Anton Frehser <hey@tonyfresher.com>
2025-12-11 21:06:58 +0100

    Add logos

--- src/app/_home/components/work.tsx
+++ src/app/_home/components/work.tsx
@@ -1,4 +1,5 @@
 import Link from '@/components/link'
+import { cn } from '@/lib/cn'
 
 const strings = {
     title: 'Work',
@@ -17,12 +18,14 @@ const strings = {
             product: 'Yandex Practicum',
             description: 'Online education for digital professions',
             period: '2021-2022',
+            logo: '/work/practicum.svg',
             link: 'https://practicum.yandex.ru'
         },
         {
             product: 'Yandex Surveys',
             description: 'Easy-to-use research toolset',
             period: '2018-2021',
+            logo: '/work/surveys.svg',
             link: 'https://surveys.yandex.com'
         }
     ]
@@ -32,11 +35,15 @@ export default function Work() {
     return (
         <div className="flex flex-col gap-10">
             <h2>{strings.title}</h2>
-            {strings.items.map(({ product, description, period, cover, link }) => (
+            {strings.items.map(({ product, description, period, cover, logo, link }) => (
                 <Link
                     key={product}
                     href={link}
-                    className="-m-4 grid rounded-md p-4 max-lg:m-0 max-lg:flex max-lg:flex-col max-lg:gap-6 max-lg:p-0 max-lg:hover:bg-transparent"
+                    className={cn(
+                        'group',
+                        '-m-4 grid grid-cols-[1fr_auto] rounded-md p-4',
+                        'max-lg:m-0 max-lg:flex max-lg:flex-col max-lg:gap-4 max-lg:p-0 max-lg:hover:bg-transparent'
+                    )}
                     display="inline-block"
                 >
                     {cover && (
@@ -52,6 +59,16 @@ export default function Work() {
                                 alt="Cover"
                             />
                         </>
+                    )}
+                    {logo && (
+                        <img
+                            className={cn(
+                                'size-14 shrink-0 opacity-95',
+                                'lg:ml-8 lg:size-24 lg:p-2 lg:group-odd:rotate-5 lg:group-even:-rotate-5'
+                            )}
+                            src={logo}
+                            alt="Logo"
+                        />
                     )}
                     <div
                         className={cn(
