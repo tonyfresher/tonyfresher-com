f2179c3d6ce445a9b479d655ef92fd870f840065
Anton Frehser <hey@tonyfresher.com>
2026-03-20 23:09:08 +0100

    Fix

--- src/app/_home/components/messenger/location-message.tsx
+++ src/app/_home/components/messenger/location-message.tsx
@@ -8,7 +8,7 @@ import { cn } from '@/lib/cn'
 
 function MapPreview() {
     return (
-        <div className="relative h-20 w-80 overflow-hidden max-sm:w-48">
+        <div className="relative h-20 w-80 overflow-hidden max-sm:h-24 max-sm:w-56">
             <Image className="object-cover" src="/about/map.png" alt="Map" fill preload />
             {/* Vignette */}
             <div className="pointer-events-none absolute inset-0 rounded-[inherit] bg-radial-[ellipse_at_center] from-transparent from-30% to-black/10" />
