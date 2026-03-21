547dee110c6430526302c7e1d60aa584e81d1a48
Anton Frehser <hey@tonyfresher.com>
2026-03-21 10:11:53 +0100

    Fix

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -3,7 +3,7 @@ import posthog from 'posthog-js'
 posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
     api_host: 'https://an.tonyfresher.com',
     ui_host: 'https://us.i.posthog.com',
-    defaults: '2025-05-24',
+    defaults: '2026-01-30',
     person_profiles: 'always',
     debug: process.env.NODE_ENV === 'development'
 })
